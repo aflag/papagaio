2845a10194be2284058a61f297ed84d347986420
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2009-01-03 07:35:25 -0200

    Kernel passou a ser carregado na higher half.

--- src/papagaio.s
+++ src/papagaio.s
@@ -15,7 +15,10 @@
 [bits 32]
 
 [extern kmain]
+[extern final_kernel]
 [global inicio]
+
+[section .comeco]
 
 ; -*- COMUNICAÇÃO COM O BOOTLOADER -*-
 MB_MAGIC equ 0x1BADB002
@@ -29,8 +32,41 @@ MB_FLAGS equ (MB_ALIGNED_FLAG | MB_MEMINFO_FLAG)
 ; -*- FIM DA COMUNICAÇÃO COM BOOTLOADER -*-
 
 inicio:
+	; Preenche o diretório de páginas com a entrada 0x19b. Assim todas
+	; entradas apontam para a página 0. Bits ligados:
+	;	* Present (bit 0)
+	;	* R/W (bit 1)
+	;	* PWT (bit 3)
+	;	* PCD (bit 4)
+	;	* PS (bit 7)
+	;	* Global (bit 8)
+	mov ecx, 0
+preenche_tabela:
+	mov eax, ecx
+	shl eax, 2
+	mov [eax], dword 0x19b
+	cmp ecx, 1024
+	inc ecx
+	jb preenche_tabela
+
+	; O diretório de páginas encontra-se no endereço físico 0x0.
+	mov eax, 0
+	mov cr3, eax
+
+	; Liga o bit no registrador cr4 que faz com que a paginação seja feita
+	; com páginas de 4MB.
+	mov edx, cr4
+	or edx, 1<<4
+	mov cr4, edx
+
+	; Liga paginação no registrador cr0.
+	mov eax, cr0
+	or eax, 0x80000000
+	mov cr0, eax
+
+	; Carrega endereço da estrutura do Multiboot
 	push ebx
 
 	call kmain
 	hlt
-	jmp $
+	jmp $-1
